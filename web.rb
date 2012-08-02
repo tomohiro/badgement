0324ec5709ef6d52d07a878646993eb338b4142c
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2012-08-02 20:44:25 +0900

    add status application

---
+++ web.rb
@@ -0,0 +1,20 @@
+# enoding: utf-8
+
+require 'sinatra'
+
+get '/' do; end
+
+get '/ci/:job' do |job|
+  begin
+    status = ENV["#{job.upcase}_STATUS"]
+    raise NameError unless status
+    content_type 'image/png'
+    send_file "public/images/#{status}.png"
+  rescue
+    "Status not found: #{job}"
+  end
+end
+
+post '/ci/:job' do |job|
+  ENV["#{job.upcase}_STATUS"] = params[:status]
+end
