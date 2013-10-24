6c823b6adbce6ff93d99905432b6d1358e6fa1df
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-10-24 22:45:32 +0900

    Fix routes

--- web.rb
+++ web.rb
@@ -9,18 +9,17 @@ configure do
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
 
-get '/' do; end
-
-get '/name/:label' do |label|
-  begin
-    status = REDIS.get(label)
-    raise NameError unless status
+get '/*' do
+  label = params[:splat].first
+  status = REDIS.get(label)
+  if status
     redirect "http://img.shields.io/#{label}/#{status}.png"
-  rescue NameError
-    redirect "http://img.shields.io/label/error.png?color=red"
+  else
+    redirect "http://img.shields.io/#{label}/undefined.png?color=red"
   end
 end
 
-post '/name/:label' do |label|
+post '/:label' do |label|
+  label = params[:splat].first
   REDIS.set(label, params[:status])
 end
